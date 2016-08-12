87ca62bf63fa4b2bc8dd43faf33fc1eef312b286
Alysha Reinard <alysha.reinard@gmail.com>
2016-08-12 15:45:00 -0600

    making progress reading in DSCOVR data

--- read_DSCOVR_data.py
+++ read_DSCOVR_data.py
@@ -23,42 +23,64 @@ def read_DSCOVR_currents():
     print("fulldir", rootdir)
     data=readsav(rootdir+"dsc_mag1.sav")
     print("dsc_mag1", data['results'][0:8])
-    dsc_mag_timetag=[]
+    print(len(data['results']))
+    dsc_mag_timetag1=[]
     dsc_mag_gsm_bx=[]
     dsc_mag_gsm_by=[]
     dsc_mag_gsm_bz=[]
+    dsc_mag_qual=[]
     
-    for line in data['results'][2:]:
+    for line in data['results']:
         vals=line.split()
 #        print("line", vals)
 
         if len(vals)==6:
-#            print((type(vals[0])))
+            date=(vals[0].decode()+' '+ vals[1].decode())
+#            print("date", date)
 #            print((type(vals[1])))
-            dsc_mag_timetag.append((vals[0].decode("utf-8")))#+' '+string(vals[1]))
+            dsc_mag_timetag1.append(date)
             dsc_mag_gsm_bx.append(vals[2])
             dsc_mag_gsm_by.append(vals[3])
             dsc_mag_gsm_bz.append(vals[4])
-    
-    print("!!!!timetag", dsc_mag_timetag)
+            dsc_mag_qual.append(vals[5])
+
 
     data=readsav(rootdir+"dsc_mag2.sav")
     print("dsc_mag2", data['results'][0:8])
+    dsc_mag_timetag2=[]
+    dsc_mag_bt=[]
+    dsc_mag_theta_gsm=[]
+    dsc_mag_phi_gsm=[]
+    dsc_mag_sample_size=[]
+    
+    for line in data['results']:
+        vals=line.split()
+#        print("line", vals)
+
+        if len(vals)==6:
+            date=(vals[0].decode()+' '+ vals[1].decode())
+#            print("date", date)
+#            print((type(vals[1])))
+            dsc_mag_timetag1.append(date)
+            dsc_mag_bt.append(vals[2])
+            dsc_mag_theta_gsm.append(vals[3])
+            dsc_mag_phi_gsm.append(vals[4])
+            dsc_mag_sample_size.append(vals[5])    
     
     data=readsav(rootdir+"dsc_plasma1.sav")
     print("dsc_plasma1", data['results'][0:8])
-    
-    data=readsav(rootdir+"dsc_plasma2.sav")
-    print("dsc_plasma2", data['results'][0:8])
-    
-    data=readsav(rootdir+"ace_mag1.sav")
-    print("ace_mag1", data['results'][0:8])
-    
-    data=readsav(rootdir+"ace_mag2.sav")
-    print("ace_mag2", data['results'][0:8])
-    
-    data=readsav(rootdir+"ace_plasma.sav")
-    print("ace_plasma", data['results'][0:8])
+#    
+#    data=readsav(rootdir+"dsc_plasma2.sav")
+#    print("dsc_plasma2", data['results'][0:8])
+#    
+#    data=readsav(rootdir+"ace_mag1.sav")
+#    print("ace_mag1", data['results'][0:8])
+#    
+#    data=readsav(rootdir+"ace_mag2.sav")
+#    print("ace_mag2", data['results'][0:8])
+#    
+#    data=readsav(rootdir+"ace_plasma.sav")
+#    print("ace_plasma", data['results'][0:8])
 
 
 #    current_a=data["current_a"]

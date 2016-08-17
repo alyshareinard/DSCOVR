3ea3a4da6721ef735fb6e9589c67bfcead2a60c0
Alysha Reinard <alysha.reinard@noaa.gov>
2016-08-17 10:38:36 -0600

    stable version that reads in ACE and DSCOVR data

--- compare_ace_dscovr.py
+++ compare_ace_dscovr.py
@@ -10,6 +10,8 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 import sys
 
+
+
 def read_rtsw_ace():
     if os.sep=="/":
         osdir=os.sep+os.path.join("Users", "alyshareinard")
@@ -20,10 +22,6 @@ def read_rtsw_ace():
     
     acelist=glob.glob(os.path.join(rootdir, '*mag_1m.txt'))
     date=[]
-    year=[]
-    month=[]
-    day=[]
-    time=[]
     s=[]
     bx=[]
     by=[]
@@ -43,24 +41,25 @@ def read_rtsw_ace():
             
 #            print("year", line[0:4])
             line=str(line)
-            print("line", line)
+#            print("line", line)
             if line[0]!=":" and line[0]!="#":
-                year=int(line[0:4])
-                month=int(line[5:7])
-                day=int(line[8:10])
-                hour=int(line[12:14])
-                minute=int(line[14:16])
-                time=int(line[12:16])
-                print(year, month, day, hour, minute)
+                line=line.split()
+                
+                year=int(line[0])
+                month=int(line[1])
+                day=int(line[2])
+                hhmm=line[3]
+                hour=int(hhmm[0:2])
+                minute=int(hhmm[2:4])
                 date.append(datetime(year, month, day, hour, minute))
             
-                s.append(line[32:38])
-                bx.append(line[39:46])
-                by.append(line[47:54])
-                bz.append(line[55:62])
-                bt.append(line[63:70])
-                lat.append(line[71:78])
-                lon.append(line[77:85])  
+                s.append(int(line[6]))
+                bx.append(float(line[7]))
+                by.append(float(line[8]))
+                bz.append(float(line[9]))
+                bt.append(float(line[10]))
+                lat.append(float(line[11]))
+                lon.append(float(line[12]))
 #            except Exception as e: 
 #                print(str(e))
           
@@ -69,13 +68,105 @@ def read_rtsw_ace():
 #    print("month", month[700:720])
 #    print("day", day[700:720])
 #    print("time", time[700:720])
-    print("s", s[700:720])
-    print("bx", bx[700:720])
-    print("by", by[700:720])
-    print("bz", bz[700:720])
-    print("bt", bt[700:720])
-    print("lat", lat[700:720])
-    print("lon", lon[700:720])
-    print("date", date[700:720])
+#    print("s", s[700:720])
+#    print("bx", bx[700:720])
+#    print("by", by[700:720])
+#    print("bz", bz[700:720])
+#    print("bt", bt[700:720])
+#    print("lat", lat[700:720])
+#    print("lon", lon[700:720])
+#    print("date", date[700:720])
+
+#    fig=plt.figure()
+    
+#    bx=fig.add_subplot
+    plt.subplot(7,1,1) 
+    plt.plot(date, bx, 'o-')
+    plt.ylim(0, 20)
+    plt.ylabel("bx")
+    plt.xticks([]) 
+    
+    plt.subplot(7,1,2) 
+    plt.plot(date, by, 'o-')
+    plt.ylim(0, 20)
+    plt.ylabel("by")
+    plt.xticks([])   
+
+    plt.subplot(7,1,3) 
+    plt.plot(date, bz, 'o-')
+    plt.ylim(0, 20)
+    plt.ylabel("bz")
+    plt.xticks([])  
+    
+    plt.subplot(7,1,4) 
+    plt.plot(date, bt, 'o-')
+    plt.ylim(0, 20)
+    plt.ylabel("bt")
+    plt.xticks([])  
+    
+    plt.subplot(7,1,5) 
+    plt.plot(date, lat, 'o-')
+    plt.ylim(-90, 90)
+    plt.ylabel("lat")
+    plt.xticks([])  
+    
+    plt.subplot(7,1,6) 
+    plt.plot(date, lon, 'o-')
+    plt.ylim(0, 360)
+    plt.ylabel("lon")
+    plt.xticks([])  
+    
+    plt.subplot(7,1,7) 
+    plt.plot(date, s, 'o-')
+    plt.ylim(0, 10)
+    plt.ylabel("s")
+    plt.xticks([])  
+    plt.show()
+    
+    
+def read_rtsw_dscovr():
+    if os.sep=="/":
+        osdir=os.sep+os.path.join("Users", "alyshareinard")
+    else:
+        osdir=os.path.join("C:"+os.sep+"Users", "alysha.reinard.SWPC")
+
+    rootdir=os.path.join(osdir, "Dropbox", "Work", "data", "DSCOVR")+os.sep
+    
+    dscovr_file=os.path.join(rootdir, 'rtsw_plot_data.txt')
+    date=[]
+    bx=[]
+    by=[]
+    bz=[]
+    bt=[]
+    lat=[]
+    lon=[]
+        
+    with open(dscovr_file, "r") as f:
+        dscovr_data=f.readlines()
+    for line in dscovr_data:
+        if line[0]!="T":
+            line=line.split()
+#            print(line)
+            ymd=line[0].split("-")
+#            print(ymd)
+            year=int(ymd[0])
+            month=int(ymd[1])
+            day=int(ymd[2])
+            hms=line[1].split(":")
+            hour=int(hms[0])
+            minute=int(hms[1])
+            sec=int(round(float(hms[2])))
+#            print(year, month, day, hour, minute, sec)
+            date.append(datetime(year, month, day, hour, minute, sec))
+#            print(date)
+            bt.append(line[2])
+            bx.append(line[3])
+            by.append(line[4])
+            bz.append(line[5])
+            lat.append(line[6])
+            lon.append(line[7])
+
 
 read_rtsw_ace()
+
+#read_rtsw_dscovr()
